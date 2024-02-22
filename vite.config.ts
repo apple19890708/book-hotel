ad253d968c0449aaab656bbf070a17a8a444ee5d
Tony <apple19890708@gmail.com>
2024-02-23 00:36:09 +0800

    [feat] change config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/book-hotel/" : "/",
+  base: "/book-hotel/",
   server: {
     proxy: {
       "/api": {
