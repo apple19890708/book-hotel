6efb6c5008e9b6a00608b89150837cdb9cfe5cf5
Tony <apple19890708@gmail.com>
2024-02-28 21:47:12 +0800

    [feat] change router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import routes from './routes';
-import { beforeEach } from './routerGuard';
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./routes";
+import { beforeEach } from "./routerGuard";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory("/book-hotel/"),
   routes: routes,
 });
 
