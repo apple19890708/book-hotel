b9fe7d9c2a798e94870033a59fc31b2e42a57519
Tony <apple19890708@gmail.com>
2024-02-28 22:37:05 +0800

    [feat] change router

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import("@/views/Home.vue"),
+    component: () => import("../views/Home.vue"),
     meta: {
       title: "首頁", //添加這部分
     },
@@ -10,7 +10,7 @@ const routes = [
   {
     path: "/rooms",
     name: "rooms",
-    component: () => import("@/views/Rooms/Index.vue"),
+    component: () => import("../views/Rooms/Index.vue"),
     meta: {
       title: "客房資訊",
     },
@@ -18,7 +18,7 @@ const routes = [
   {
     path: "/rooms/:id",
     name: "rooms-item",
-    component: () => import("@/views/Rooms/Item.vue"),
+    component: () => import("../views/Rooms/Item.vue"),
     meta: {
       title: "客房資訊",
     },
@@ -26,7 +26,7 @@ const routes = [
   {
     path: "/rooms/:id/reservation",
     name: "rooms-reservation",
-    component: () => import("@/views/Rooms/Reservation.vue"),
+    component: () => import("../views/Rooms/Reservation.vue"),
     meta: {
       title: "客房預約",
       requiresAuth: true,
@@ -35,7 +35,7 @@ const routes = [
   {
     path: "/rooms/:id/success",
     name: "rooms-success",
-    component: () => import("@/views/Rooms/ReservationSuccess.vue"),
+    component: () => import("../views/Rooms/ReservationSuccess.vue"),
     meta: {
       title: "客房預約成功",
       requiresAuth: true,
@@ -43,12 +43,12 @@ const routes = [
   },
   {
     path: "/",
-    component: () => import("@/components/Layouts/Login.vue"),
+    component: () => import("../components/Layouts/Login.vue"),
     children: [
       {
         path: "login",
         name: "login",
-        component: () => import("@/views/Login.vue"),
+        component: () => import("../views/Login.vue"),
         meta: {
           title: "會員登入",
         },
@@ -56,7 +56,7 @@ const routes = [
       {
         path: "signup",
         name: "signup",
-        component: () => import("@/views/Signup.vue"),
+        component: () => import("../views/Signup.vue"),
         meta: {
           title: "會員註冊",
         },
@@ -66,7 +66,7 @@ const routes = [
   {
     path: "/user",
     name: "user",
-    component: () => import("@/views/User/Index.vue"),
+    component: () => import("../views/User/Index.vue"),
     meta: {
       title: "會員資訊",
       requiresAuth: true,
@@ -75,7 +75,7 @@ const routes = [
   {
     path: "/user/orders",
     name: "user-orders",
-    component: () => import("@/views/User/Orders.vue"),
+    component: () => import("../views/User/Orders.vue"),
     meta: {
       title: "會員訂單",
       requiresAuth: true,
@@ -84,7 +84,7 @@ const routes = [
   {
     path: "/guideline",
     name: "guideline",
-    component: () => import("@/views/Guideline.vue"),
+    component: () => import("../views/Guideline.vue"),
     meta: {
       title: "Guildline",
     },
@@ -92,7 +92,7 @@ const routes = [
   {
     path: "/:pathMath(.*)*",
     name: "404",
-    component: import("@/views/NotFound.vue"),
+    component: import("../views/NotFound.vue"),
     meta: {
       title: "404",
     },
